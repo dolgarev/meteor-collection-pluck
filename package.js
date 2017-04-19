35ced9a72d6c78b8a629b38f14544e3bfc711a62
dolgarev <o.dolgarev@gmail.com>
2017-04-19 10:45:17 +0300

    updated deps

--- package.js
+++ package.js
@@ -7,6 +7,6 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.use(['mongo']);
+  api.use(['mongo', 'random']);
   api.addFiles('pluck.js', ['client', 'server']);
 });
