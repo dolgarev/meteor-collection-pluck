5a1f31c7adef5a3cfa563b3e748bf639883a8363
dolgarev <o.dolgarev@gmail.com>
2017-04-29 20:36:59 +0300

    minor improvements

--- pluck.js
+++ pluck.js
@@ -1,19 +1,21 @@
 (function() {
-  var fakeCollection = new Mongo.Collection("__fake_collection__" + Random.id());
+  if (Meteor.isServer) {
+    var fakeCollection = new Mongo.Collection("__fake_collection__" + Random.id());
+    fakeCollection.findOne();
+    var cursor = Object.getPrototypeOf(fakeCollection.find(null)).constructor
+  } else {
+    var cursor = Mongo.Cursor
+  }
 
-  fakeCollection.findOne();
+  if (cursor.prototype.pluck) return
 
-  var cursor = Object.getPrototypeOf(fakeCollection.find(null)).constructor;
-
-  if (!(cursor.pluck)) {
-    cursor.prototype.pluck = function(prop) {
-      if (prop == null) {
-        return [];
-      } else {
-        return this.map(function(item) {
-          return item[prop];
-        });
-      }
+  cursor.prototype.pluck = function (prop) {
+    if (prop == null) {
+      return []
+    } else {
+      return this.map(function (item) {
+        return item[prop]
+      })
     }
   }
 })();
